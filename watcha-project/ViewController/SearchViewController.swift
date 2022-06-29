95ebb0aef83543ac6ae880fdfbf5af3461f4aa83
DeokHo98 <ekdj98@gmail.com>
2022-06-29 14:21:41 +0900

    feat: searchButton 생성

--- watcha-project/ViewController/SearchViewController.swift
+++ watcha-project/ViewController/SearchViewController.swift
@@ -13,10 +13,26 @@ class SearchViewController: UIViewController {
     
     private let searchTextField: UITextField = {
         let tf = UITextField()
-        tf.placeholder = "Search GIPHY"
+        tf.placeholder = " Search GIPHY"
         tf.backgroundColor = .white
         tf.tintColor = .darkGray
         return tf
+    }()
+    private let searchButton: UIButton = {
+        let bt = UIButton(type: .system)
+        bt.setImage(UIImage(systemName: "magnifyingglass"), for: .normal)
+        bt.tintColor = .white
+        let gradient = CAGradientLayer()
+        gradient.colors = [UIColor.systemPink.cgColor,UIColor.systemPurple.cgColor]
+        gradient.locations = [0,1]
+        gradient.startPoint = CGPoint(x: 1.0, y: 0.0)
+        gradient.endPoint = CGPoint(x: 0.0, y: 1.0)
+        gradient.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+        bt.layer.insertSublayer(gradient, at: 0)
+        if let imageView = bt.imageView {
+            bt.bringSubviewToFront(imageView)
+        }
+        return bt
     }()
     
     //MARK: - LifeCycle
@@ -29,6 +45,7 @@ class SearchViewController: UIViewController {
     }
     
     //MARK: - HelperFunction
+    
     private func setAttribute() {
         view.backgroundColor = .black
     }
@@ -39,6 +56,11 @@ class SearchViewController: UIViewController {
         [searchTextField].forEach {
             view.addSubview($0)
         }
-        searchTextField.anchor(top: view.safeAreaLayoutGuide.topAnchor,leading: view.leadingAnchor,trailing: view.trailingAnchor,height: 50)
+        searchTextField.anchor(
+            top: view.safeAreaLayoutGuide.topAnchor,
+            leading: view.leadingAnchor,
+            trailing: searchButton.leadingAnchor,
+            height: 50
+        )
     }
 }
