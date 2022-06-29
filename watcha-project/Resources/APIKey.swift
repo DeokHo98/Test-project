4a303503e100bb089adce6e77bd831138f1af19a
DeokHo98 <ekdj98@gmail.com>
2022-06-30 00:00:38 +0900

    feat: API KEY 생성

---
+++ watcha-project/Resources/APIKey.swift
@@ -0,0 +1,12 @@
+//
+//  APIKey.swift
+//  watcha-project
+//
+//  Created by 정덕호 on 2022/06/29.
+//
+
+import Foundation
+
+struct APIKey {
+    static let key = "api_key=MjCPlYY5U7JKYjuCuWac3SSmrropRpI1"
+}
