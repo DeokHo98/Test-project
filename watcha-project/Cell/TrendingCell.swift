354ac8d9115dca36ddc2d2922ad4d2befd0ba630
DeokHo98 <ekdj98@gmail.com>
2022-06-29 14:59:44 +0900

    feat: trendingCell 생성

--- watcha-project/Cell/TrendingCell.swift
+++ watcha-project/Cell/TrendingCell.swift
@@ -9,4 +9,19 @@ import UIKit
 
 class TrendingCell: UICollectionViewCell {
     
+    //MARK: - Identifier
+    
+    static let identifier = "TrendingCell"
+    
+    //MARK: -  lifeCycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .blue
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 }
