0a0c1c5a4179bc8732b3fe69661f20eea3f99d51
DeokHo98 <ekdj98@gmail.com>
2022-06-29 13:09:04 +0900

    Initial Commit

---
+++ watcha-project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  watcha-project
+//
+//  Created by 정덕호 on 2022/06/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
