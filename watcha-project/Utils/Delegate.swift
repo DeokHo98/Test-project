70c8f162423f3c24927e7e5c4590eb1005506db7
DeokHo98 <ekdj98@gmail.com>
2022-06-30 15:59:50 +0900

    자동완성 키워드를 전달하는 델리게이트 패턴 구현

---
+++ watcha-project/Utils/Delegate.swift
@@ -0,0 +1,12 @@
+//
+//  Delegate.swift
+//  watcha-project
+//
+//  Created by 정덕호 on 2022/06/30.
+//
+
+import Foundation
+
+protocol keyWordDelegate: AnyObject {
+    func didSelectRowAt(keyword: String)
+}
