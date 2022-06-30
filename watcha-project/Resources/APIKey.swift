0ff3bca9f7a1f88c7efca5ceaf583ff9b88a2efc
DeokHo98 <ekdj98@gmail.com>
2022-06-30 14:09:12 +0900

    URL을 APIURL로  분리해서 관리

--- watcha-project/Resources/APIKey.swift
+++ watcha-project/Resources/APIKey.swift
@@ -10,3 +10,19 @@ import Foundation
 struct APIKey {
     static let key = "api_key=MjCPlYY5U7JKYjuCuWac3SSmrropRpI1"
 }
+
+enum APIURL {
+    var url: String {
+        switch self {
+        case .mostPopular:
+            return "https://api.giphy.com/v1/gifs/trending?\(APIKey.key)&limit=20&rating=g"
+        case .trendKeyword:
+            return "https://api.giphy.com/v1/trending/searches?\(APIKey.key)"
+        case .autocompleteKeword:
+            return "https://api.giphy.com/v1/gifs/search/tags?\(APIKey.key)&limit=10"
+        }
+    }
+    case mostPopular
+    case trendKeyword
+    case autocompleteKeword
+}
