a22f378ce5dad70cf0c0721e8cb8ce4cab71b49c
DeokHo98 <ekdj98@gmail.com>
2022-07-01 13:14:44 +0900

    style: 필요없는 코드 삭제

--- watcha-project/Utils/ImageLoader.swift
+++ watcha-project/Utils/ImageLoader.swift
@@ -35,19 +35,3 @@ struct ImageLoader {
         }
     }
 }
-
-
-extension UIImage {
-
-    static func animatedGif(from data: Data, framesPerSecond: Double = 10) -> UIImage? {
-        guard let source = CGImageSourceCreateWithData(data as CFData, nil) else { return nil }
-        let imageCount = CGImageSourceGetCount(source)
-        var images: [UIImage] = []
-        for i in 0 ..< imageCount {
-            if let cgImage = CGImageSourceCreateImageAtIndex(source, i, nil) {
-                images.append(UIImage(cgImage: cgImage))
-            }
-        }
-        return UIImage.animatedImage(with: images, duration: Double(images.count) / framesPerSecond)
-    }
-}
