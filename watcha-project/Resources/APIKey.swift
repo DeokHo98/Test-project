42ed0bb6b496226a5b85fd5792c090f3c3589d48
DeokHo98 <ekdj98@gmail.com>
2022-07-01 13:33:59 +0900

    style: test파일 삭제

--- watcha-project/Resources/APIKey.swift
+++ watcha-project/Resources/APIKey.swift
@@ -7,21 +7,20 @@
 
 import Foundation
 
-struct APIKey {
-    static let key = "api_key=MjCPlYY5U7JKYjuCuWac3SSmrropRpI1"
-}
+private let APIKey = "api_key=MjCPlYY5U7JKYjuCuWac3SSmrropRpI1"
 
 enum APIURL {
+
     var url: String {
         switch self {
         case .mostPopular:
-            return "https://api.giphy.com/v1/gifs/trending?\(APIKey.key)&limit=20&rating=g"
+            return "https://api.giphy.com/v1/gifs/trending?\(APIKey)&limit=20&rating=g"
         case .trendKeyword:
-            return "https://api.giphy.com/v1/trending/searches?\(APIKey.key)"
+            return "https://api.giphy.com/v1/trending/searches?\(APIKey)"
         case .autocompleteKeword:
-            return "https://api.giphy.com/v1/gifs/search/tags?\(APIKey.key)&limit=10"
+            return "https://api.giphy.com/v1/gifs/search/tags?\(APIKey)&limit=10"
         case .search:
-            return "https://api.giphy.com/v1/gifs/search?\(APIKey.key)&limit=20&rating=g&q="
+            return "https://api.giphy.com/v1/gifs/search?\(APIKey)&limit=20&rating=g&q="
         }
     }
     case mostPopular
